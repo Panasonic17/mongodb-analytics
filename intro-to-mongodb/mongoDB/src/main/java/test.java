c1f9f533e576fe09bd9fb2204dda4f309df0a67e
oleksandr_shainoga <oleksandr_shainoga@epam.com>
2017-12-29 17:33:01 +0200

    lash

--- intro-to-mongodb/mongoDB/src/main/java/test.java
+++ intro-to-mongodb/mongoDB/src/main/java/test.java
@@ -1,8 +1,12 @@
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.*;
+import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.ListDatabasesIterable;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 
 import java.net.UnknownHostException;
+import java.util.Set;
 
 /**
  * Created by Oleksandr_Shainoga on 12/29/2017.
@@ -10,9 +14,13 @@ import java.net.UnknownHostException;
 public class test {
     public static void main(String[] args) throws UnknownHostException {
         MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://"+HidenData.login+":"+HidenData.pass+"@mflix-ankqa.mongodb.net/test");
-
+                "mongodb+srv://" + HidenData.login + ":" + HidenData.pass + "@mflix-ankqa.mongodb.net/test");
         MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoDatabase database = mongoClient.getDatabase("mflix");
+        ListCollectionsIterable<Document> collectionsIterable = database.listCollections();
+        for (Document d : collectionsIterable) {
+            System.out.println("asd");
+            System.out.println(d);
+        }
     }
 }
